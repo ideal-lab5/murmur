9444bb070caff547b518cfc5decc7aa43ee06d72
driemworks <tonyrriemer@gmail.com>
2024-03-11 21:28:11 -0500

    integrate merkle proof

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,14 @@ mod otp;
 use std::io::{Read, Write, BufRead, BufReader};
 use std::fs::File;
 use std::time::Duration;
+use std::collections::HashMap;
+
 use clap::{Args, Parser, Subcommand};
 
 use ckb_merkle_mountain_range::{
-    MMR, Merge, Result as MMRResult,
-    util::{MemMMR, MemStore},
+    MerkleProof,
+    MMR, Merge, Result as MMRResult, MMRStore,
+    util::{ MemMMR, MemStore },
 };
 
 use rand_chacha::{
@@ -28,6 +31,7 @@ use etf_crypto_primitives::{
     ibe::fullident::BfIbe,
     client::etf_client::{AesIbeCt, DefaultEtfClient, EtfClient},
     utils::{convert_to_bytes, hash_to_g1},
+
 };
 
 use sp_keyring::AccountKeyring;
@@ -37,7 +41,7 @@ use frame_support::{BoundedVec, traits::ConstU32};
 use std::time::Instant;
 use indicatif::ProgressBar;
 
-use codec::Encode;
+use codec::{Decode, Encode};
 
 /// Command line
 #[derive(Parser)]
@@ -81,7 +85,7 @@ struct WalletExecuteDetails {
 use sp_core::Bytes;
 use sha3::Digest;
 
-#[derive(Eq, PartialEq, Clone, Debug, Default)]
+#[derive(Eq, PartialEq, Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
 struct Leaf(pub Vec<u8>);
 impl From<Vec<u8>> for Leaf {
     fn from(data: Vec<u8>) -> Self {
@@ -104,6 +108,21 @@ impl Merge for MergeLeaves {
         Ok(Leaf(hash.to_vec().into()))
     }
 }
+
+
+fn load_leaves() -> Vec<(u64, Leaf)> {
+    let mmr_store_file = File::open("mmr_store").expect("Unable to open file");
+    let leaves: Vec<(u64, Leaf)> = serde_cbor::from_reader(mmr_store_file).unwrap();
+    leaves
+}
+
+// Write the ciphertext to a file
+fn write_leaves(leaves: &[(u64, Leaf)]) {
+   let mut mmr_store_file = File::create("mmr_store").expect("should be ok");
+   serde_cbor::to_writer(mmr_store_file, &leaves).unwrap();
+}
+
+
 // /// the runtime call type
 // type Call = runtime_types::node::RuntimeCall;
 // /// the balances call type
@@ -143,41 +162,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             // write MMR leaves to a file
             let mut mmr_store_file = File::create("mmr_store").unwrap();
-
+ 
             let store = MemStore::default();
             let mut mmr = MemMMR::<_, MergeLeaves>::new(0, store);
 
-            println!("Generating Codes");
-            let mut ciphertexts = Vec::new();
+            println!("Generating MMR");
+            let mut leaf_data = Vec::new();
             for i in 1..101 {
                 let id = (current_slot_number + i).to_string().as_bytes().to_vec();
                 let otp_code = totp.generate((current_slot_number + i) as u32);
                 // TODO: Do we need to encrypt the entire otp code or just a hash of it?
                 // AesIbeCt
-                let ct = DefaultEtfClient::<BfIbe>::encrypt(
-                    ibe_params.1.clone(),
-                    ibe_params.2.clone(),
-                    &otp_code.as_bytes(), 
-                    vec![id],
-                    1,
-                    &mut rng,
-                ).unwrap();
+                // let ct = DefaultEtfClient::<BfIbe>::encrypt(
+                //     ibe_params.1.clone(),
+                //     ibe_params.2.clone(),
+                //     &otp_code.as_bytes(), 
+                //     vec![id.clone()],
+                //     1,
+                //     &mut rng,
+                // ).unwrap();
                 
 
-                let s = Ciphertext {
-                    ciphertext: <BoundedVec<_, ConstU32<512>>>::truncate_from(ct.aes_ct.ciphertext),
-                    nonce: <BoundedVec<_, ConstU32<96>>>::truncate_from(ct.aes_ct.nonce),
-                    capsule: <BoundedVec<_, ConstU32<512>>>::truncate_from(ct.etf_ct[0].clone()),
-                };
+                // let s = Ciphertext {
+                //     ciphertext: <BoundedVec<_, ConstU32<512>>>::truncate_from(ct.aes_ct.ciphertext),
+                //     nonce: <BoundedVec<_, ConstU32<96>>>::truncate_from(ct.aes_ct.nonce),
+                //     capsule: <BoundedVec<_, ConstU32<512>>>::truncate_from(ct.etf_ct[0].clone()),
+                // };
 
-                // mmr_store_file.write_all(&s.encode());
-                // mmr_store_file.write_all(b"\n");
-                let _ = mmr.push(Leaf::from(s.encode()));
-                ciphertexts.push(s);
+                let leaf = Leaf::from(otp_code.as_bytes().to_vec());
+                // TODO: error handling
+                let pos = mmr.push(leaf.clone()).unwrap();
+                leaf_data.push((current_block.header().number() as u64 + i, leaf));
             }
 
-            write_leaves(&ciphertexts);
+            write_leaves(&leaf_data);
             let root = mmr.get_root().expect("The MMR root should be calculable");
+            println!("MMR ROOT {:?}", root);
 
             let name = args.name.as_bytes().to_vec();
 
@@ -190,17 +210,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let events = etf
                 .tx()
                 .sign_and_submit_then_watch_default(&create_anon_tx, &from)
-                .await?
-                .wait_for_finalized_success()
                 .await?;
-
-            println!("Submitted transaction");
+            println!("OTP wallet creation successful!");
+            bar.finish();
+            // TODO: get the wallet address
             // Find a Transfer event and print it.
-            let transfer_event = events.find_first::<etf::otp::events::OtpProxyCreated>()?;
-            if let Some(_event) = transfer_event {
-                println!("OTP wallet creation successful!");
-                bar.finish();
-            }
+            // let transfer_event = events.find_first::<etf::otp::events::OtpProxyCreated>()?;
+            // if let Some(_event) = transfer_event {
+            //     println!("OTP wallet creation successful!");
+            //     bar.finish();
+            // }
         },
         Commands::Execute(args) => {
             println!("OTP Wallet Client: Execute Wallet Balance Transfer");
@@ -216,31 +235,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut rng = ChaCha20Rng::seed_from_u64(1);
             let mut block_subscription = etf.blocks().subscribe_all().await?;
             if let Some(Ok(current_block)) = block_subscription.next().await {
+                // get data from runtime storage
                 let current_slot_number: u64 = current_block.storage()
                     .fetch(&etf::storage().aura().current_slot())
-                    .await?.unwrap().0;
+                    .await?.unwrap().0; 
+                println!("current slot no {:?}", current_slot_number);
                 let ibe_params = current_block.storage()
                     .fetch(&etf::storage().etf().ibe_params())
                     .await?.unwrap();
-                // Build a balance transfer extrinsic.
-                // let dest = dev::bob().public_key().into();
+                
+                // build balance transfer
                 let bob = AccountKeyring::Bob.to_account_id().into();
-
+                // get the value argument
                 let v: u128 = args.amount
                     .split_whitespace()
                     .map(|r| r.replace('_', "").parse().unwrap())
                     .collect::<Vec<_>>()[0];
-
-                let balance_transfer_call = RuntimeCall::Balances(BalancesCall::transfer_allow_death {
-                    dest: bob,
-                    value: v,
+                let balance_transfer_call = RuntimeCall::Balances(
+                    BalancesCall::transfer_allow_death {
+                        dest: bob,
+                        value: v,
                 });
 
+                let id = (current_slot_number + args.delay).to_string().as_bytes().to_vec();
+
+                // calculate the OTP code we need
                 let otp_code = totp.generate((current_slot_number + args.delay - 1) as u32);
                 println!("Will execute at {:?} using code {:?} ", current_block.header().number + (args.delay as u32), otp_code);
-
-                let bounded = <BoundedVec<_, ConstU32<32>>>::truncate_from(args.name.as_bytes().to_vec());
-                let id = (current_slot_number + args.delay).to_string().as_bytes().to_vec();
 
                 let expected_otp_ciphertext = DefaultEtfClient::<BfIbe>::encrypt(
                     ibe_params.1.clone(),
@@ -252,26 +273,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ).unwrap();
 
                 let expected_otp_ct = Ciphertext {
-                    ciphertext: <BoundedVec<_, ConstU32<512>>>::truncate_from(expected_otp_ciphertext.aes_ct.ciphertext),
+                    ciphertext: <BoundedVec<_, ConstU32<620>>>::truncate_from(expected_otp_ciphertext.aes_ct.ciphertext),
                     nonce: <BoundedVec<_, ConstU32<96>>>::truncate_from(expected_otp_ciphertext.aes_ct.nonce),
                     capsule: <BoundedVec<_, ConstU32<512>>>::truncate_from(expected_otp_ciphertext.etf_ct[0].clone()),
                 };
 
-                // prepare the merkle proof
-                let mut mmr = load_mmr();
-                // get the position of the expected otp ciphertext
-                let pos = mmr.push(Leaf::from(expected_otp_ct.encode())).unwrap();
-                let proof = mmr.gen_proof(vec![pos]).expect("should be ok");
+                let bounded = <BoundedVec<_, ConstU32<32>>>::truncate_from(args.name.as_bytes().to_vec());
+
+                let target_block_number: u64 = (current_block.header().number + 2) as u64;
+                let leaves: Vec<(u64, Leaf)> = load_leaves();
+
+                // rebuild the MMR and search for the position of the leaf for the given block number
+                let store = MemStore::default();
+                let mut mmr = MemMMR::<_, MergeLeaves>::new(0, store);
+                let mut target_pos: u64 = 0;
+                leaves.into_iter().for_each(|leaf_data| {
+                    let leaf = leaf_data.1;
+                    let block_num = leaf_data.0;
+                    let pos = mmr.push(leaf.clone()).unwrap();
+                    if block_num == target_block_number {
+                        target_pos = pos;
+                    }
+                });
+
+                // prepare merkle proof
+                let root = mmr.get_root().expect("The MMR root should be calculable");
+                let proof = mmr.gen_proof(vec![target_pos]).expect("should be ok");
+                proof.verify(root, vec![(target_pos, Leaf::from(expected_otp_hash.to_vec()))]).unwrap(),
+
                 let proof_items: Vec<Vec<u8>> = proof.proof_items().iter()
                     .map(|leaf| leaf.0.to_vec().clone())
                     .collect::<Vec<_>>();
-
-                // println!("Successfully created the Merkle proof");
+               
+                // build proxy call
                 let proxy_call = RuntimeCall::OTP(OtpCall::proxy {
                     name: bounded,
-                    otp: otp_code.as_bytes().to_vec().as_slice().try_into().expect("should be ok"),
+                    otp: otp_code
+                        .as_bytes()
+                        .to_vec()
+                        .as_slice()
+                        .try_into()
+                        .expect("should be 6 digit OTP code"),
+                    position: target_pos,
                     ciphertext: expected_otp_ct,
-                    position: pos,
                     proof: proof_items,
                     call: Box::new(balance_transfer_call),
                 });
@@ -285,24 +329,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     &mut rng,
                 ).unwrap();
 
+                let at: u32 = current_block.header().number + args.delay as u32;
+                let ct = etf::runtime_types::pallet_etf::Ciphertext {
+                    ciphertext: 
+                        etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.aes_ct.ciphertext),
+                    nonce: 
+                        etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.aes_ct.nonce),
+                    capsule: 
+                        etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.etf_ct[0].clone())
+                };
+
+                // println!("ct {:?}", ct);
+
                 let sealed_tx = etf::tx()
                     .scheduler()
-                    .schedule_sealed(
-                        current_block.header().number + args.delay as u32, 127, 
-                        etf::runtime_types::pallet_etf::Ciphertext {
-                            ciphertext: 
-                                etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.aes_ct.ciphertext),
-                            nonce: 
-                                etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.aes_ct.nonce),
-                            capsule: 
-                                etf::runtime_types::bounded_collections::bounded_vec::BoundedVec(timelocked_proxy_call.etf_ct[0].clone()),
-                });
+                    .schedule_sealed(at, 127, ct);
 
                 let events = etf
                     .tx()
                     .sign_and_submit_then_watch_default(&sealed_tx, &dev::alice())
-                    .await?
-                    .wait_for_finalized_success()
                     .await?;
             }
             bar.finish();
@@ -315,41 +360,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn load_mmr() -> MemMMR<Leaf, MergeLeaves> {
-    let store = MemStore::default();
-    let mut mmr = MemMMR::<_, MergeLeaves>::new(0, store);
-    let file = File::open("mmr_store").expect("Unable to open file");
-    let mut reader = BufReader::new(file);
-    let mut file_contents = Vec::new();
-
-    reader.read_to_end(&mut file_contents).expect("Unable to read file");
-
-    let mut start = 0;
-    while let Some(mut end) = file_contents[start..].iter().position(|&x| x == b'\n') {
-        end += start;
-        let leaf = Leaf::from(file_contents[start..end].to_vec());
-        let _ = mmr.push(leaf).expect("Unable to push Leaf");
-        start = end + 1;
-    }
-
-    mmr
+#[cfg(test)]
+mod tests {
+    // pub fn test_can_read_write_leaves() {
+        
+    // }
 }
-
-// Write the ciphertext to a file
-// TODO error handling
-fn write_leaves(ciphertexts: &[Ciphertext]) {
-   let mut mmr_store_file = File::create("mmr_store").expect("should be ok");
-    
-    for s in ciphertexts {
-        // Serialize the struct to CBOR format
-        // let encoded = serde_json::to_vec(s)?;
-        mmr_store_file.write_all(&s.encode()).expect("should be ok");
-        mmr_store_file.write_all(b"\n").expect("should be ok");
-    }
-    
-}
-
-// /// convert a u64 to a Vec
-// fn get_slot(i: u64) -> Vec<u8> {
-//     i.to_string().as_bytes().to_vec()
-// }
